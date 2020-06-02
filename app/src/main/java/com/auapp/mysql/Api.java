1bdc6c0a9b1d06b6d0637a4fd58bb2ff8d39ba15
Abhijeet Shinde13 <33342683+abhijeet-shinde264@users.noreply.github.com>
2020-06-02 21:04:07 +0530

    Second commit

--- app/src/main/java/com/auapp/mysql/Api.java
+++ app/src/main/java/com/auapp/mysql/Api.java
@@ -6,11 +6,11 @@ import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 class Api {
-    @POST("register.php")
+    @POST("http://192.168.0.7/android-users/register.php")
     @FormUrlEncoded
     Call<Model> register(@Field("username") String username, @Field("email") String email, @Field("password") String password) ;
 
-    @POST("login.php")
+    @POST("http://192.168.0.7/android-users/login.php")
     @FormUrlEncoded
     Call<Model> login(@Field("username") String username, @Field("password") String password) ;
 }
